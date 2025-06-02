7f558648db243df9bdeedbe27aa8b662552be1d2
vlad_uve <vladislav.yushkevich.uve@gmail.com>
2025-06-02 01:12:55 +0000

    Update plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import torch
 from IPython.display import Image, display
 
-def plot_experiment_history(loss_list, label_list, title, to_plot_train=False, color=plt.get_cmap('tab10').colors):
+def plot_experiment_history(loss_list, label_list, to_plot_train=False, color=plt.get_cmap('tab10').colors):
     '''
     Plot loss curves for multiple models.
 
